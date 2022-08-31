b1581194decd1ebed4cb2f3bf2f521e11b161435
Michael <mbugyis@butler.edu>
2022-08-31 17:25:55 -0400

    app and flask connection

---
+++ app.py
@@ -0,0 +1,10 @@
+# import dependency
+from flask import Flask
+
+# Create a New Flask App
+app = Flask(__name__)
+
+# Create Flask Routes
+@app.route('/')
+def hello_world():
+    return "Hello world"
